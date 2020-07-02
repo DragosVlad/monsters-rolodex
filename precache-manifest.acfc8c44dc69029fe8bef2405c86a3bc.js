0abfb5e7651312ec97685fd473ee4d5c57398cdb
Dragos Vlad <dragos.vlad95@gmail.com>
2020-07-02 14:35:50 +0300

    Updates

---
+++ precache-manifest.acfc8c44dc69029fe8bef2405c86a3bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06aefedc061bf9805e89bfcc759c8e80",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "21c32c37a23e89e32fe8",
+    "url": "/monsters-rolodex/static/css/main.83b39352.chunk.css"
+  },
+  {
+    "revision": "0d97cbec396c8790c278",
+    "url": "/monsters-rolodex/static/js/2.2d23d57e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.2d23d57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c32c37a23e89e32fe8",
+    "url": "/monsters-rolodex/static/js/main.553da201.chunk.js"
+  },
+  {
+    "revision": "61952599595659462710",
+    "url": "/monsters-rolodex/static/js/runtime-main.d18d304e.js"
+  }
+]);
